30bb40991c1f5c1ba6dfb3d0bb9781e5e14e19c6
denys <denys.potapov@gmail.com>
2019-01-17 02:24:54 +0200

    update mailchimp api

--- php/subscribe.php
+++ php/subscribe.php
@@ -1,42 +1,43 @@
 <?php
 
-  $config = require 'config.php';
+$config = require 'config.php';
 
-  $apiKey         = $config['apiKey'];
-  $listId         = $config['listId'];
-  $double_optin   = false;
-  $send_welcome   = true;
-  $email_type     = 'html';
-  $email          = $_POST['email'];
-  $name          = $_POST['name'];
+$apiKey         = $config['apiKey'];
+$listId         = $config['listId'];
+$double_optin   = false;
+$send_welcome   = true;
+$email_type     = 'html';
+$email          = $_POST['email'];
+$name           = $_POST['name'];
 
-  // Replace us8 with your datacentre, usually found at end of api key
-  $submit_url     = "http://us12.api.mailchimp.com/1.3/?method=listSubscribe";
+// Replace us8 with your datacentre, usually found at end of api key
+$submit_url     = "https://us12.api.mailchimp.com/3.0";
+$members_url    = $submit_url . "/lists/" . $listId . "/members";
 
-  $data = array(
-      'email_address'=>$email,
-      'merge_vars' => array('NAME'=>$name),
-      'apikey'=>$apiKey,
-      'id' => $listId,
-      'double_optin' => $double_optin,
-      'send_welcome' => $send_welcome,
-      'email_type' => $email_type
-  );
+$data = [
+    'email_address' => $email,
+    'status' => 'subscribed',
+    'email_type' => $email_type,
+    'merge_vars' => ['NAME'=>$name]
+];
 
-  $payload = json_encode($data);
-   
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, $submit_url);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_POST, true);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, urlencode($payload));
-   
-  $result = curl_exec($ch);
-  curl_close($ch);
-  $data = json_decode($result);
+$payload = json_encode($data);
+ 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $members_url);
+curl_setopt($ch, CURLOPT_USERPWD, "apikey:" . $apiKey);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-    if ($data->error) {
-        echo '<p class="sub-form-error"><i class="fa fa-exclamation-triangle"></i>'.$data->error.'</p>';
-    } else {
-        echo "<p class='sub-form-success'><i class='fa fa-envelope'></i>Дякуємо! Чекайте на новини!</p>";
-    }
+$result = curl_exec($ch);
+$data = json_decode($result);
+
+if ($data->status == 400) {
+    echo '<p class="sub-form-error"><i class="fa fa-exclamation-triangle"></i>'.$data->detail.'</p>';
+} else {
+    echo "<p class='sub-form-success'><i class='fa fa-envelope'></i>Дякуємо! Чекайте на новини!</p>";
+}
+
+curl_close($ch);
